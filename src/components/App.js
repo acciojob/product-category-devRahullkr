8eb9d28f2afbb22e592d02d46f800b95c48132b9
Rahulllkr <rahulkumar8604745@gmail.com>
2026-03-19 17:15:38 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
   let male = ["Shirts","Facial-grooming kits","Hair styling","Jeans","Shoes"]
   let female = ["Tops","Skirts","Makeup items","Jewellery","Sandals"]
+  const [data,setData] = useState([])
+  const [category,setCategory] = useState("")
+  useEffect(() => {
+    if(category === "male"){
+      setData(male)
+    }else if(category === "female"){
+      setData(female)
+    }else{
+      setData([])
+    }
+  },[category])
   return (
     <div>
         {/* Do not remove the main div */}
+        <select onChange={(e) => setCategory(e.target.value)}>
+          <option value="">Select Category</option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+        </select>
+        <ul>
+          {
+            data.map((item,index) => <li key={index}>{item}</li>)
+          }
+        </ul>
     </div>
   )
 }

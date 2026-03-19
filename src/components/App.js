58b475b030ab17902a3f7b9a8dab9d2f0ef9315a
Rahulllkr <rahulkumar8604745@gmail.com>
2026-03-19 17:04:36 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  let male = ["Shirts","Facial-grooming kits","Hair styling","Jeans","Shoes"]
+  let female = ["Tops","Skirts","Makeup items","Jewellery","Sandals"]
   return (
     <div>
         {/* Do not remove the main div */}
